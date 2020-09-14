9311cccddc697f9ab4c3baa74c5de7d86e64efcc
Streeterxs <afonso_mg3@hotmail.com>
2020-09-14 15:20:03 -0300

    Tailwind Configs - New Spacings

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,20 @@ module.exports = {
       '6': '6px',
       '8': '8px',
     },
-    extend: {},
+    extend: {
+      spacing: {
+        '72': '18rem',
+        '84': '21rem',
+        '96': '24rem',
+        '108': '27rem',
+        '120': '30rem',
+        '132': '33rem',
+        '144': '36rem',
+        '156': '39rem',
+        '178': '42rem',
+        '190': '45rem',
+      }
+    },
   },
   variants: {},
   plugins: [
